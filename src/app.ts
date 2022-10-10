cac558dbaed11e523b67c34c529b05ff1233e810
vkaraca <vkaraca@akimmetal.com.tr>
2022-10-10 21:34:41 +0300

    Add helmet, rate limit, ip check and error handler

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express, { Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import { json } from 'body-parser';
+import helmet from 'helmet';
 import { router } from './routes/index';
 import { errorLogger, logger, requestLogger } from './services/logger-service';
+import { errorHandler } from './middleware/error-handler';
+import { ipCheck } from './middleware/ip-allow-list';
 
 const app = express();
 
 app.disable('x-powered-by');
 app.set('trust proxy', true);
 app.use(json());
+app.use(helmet());
 app.use(requestLogger);
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(ipCheck);
 app.use('/api', router);
 
 app.all('*', async (req: Request, res: Response) => {
@@ -17,5 +24,7 @@ app.all('*', async (req: Request, res: Response) => {
 });
 
 app.use(errorLogger);
+
+app.use(errorHandler);
 
 export { app };
